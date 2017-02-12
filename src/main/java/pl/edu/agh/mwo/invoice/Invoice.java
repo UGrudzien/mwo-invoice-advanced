edc4c020ab7c7729a8d335f05da18967bad76ed8
Student4 <Student4@S7N-PROGLANG-2.pcoip.ki.agh.edu.pl>
2017-02-12 12:23:46 +0100

    Invoice

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,27 +1,57 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
+	private HashMap<Product, Integer> products = new HashMap<Product, Integer>();
 	public void addProduct(Product product) {
-		// TODO: implement
+		if (products.containsKey(product)){
+			products.put(product, products.get(product)+1);
+		}
+		else{
+			products.put(product, 1);
+		}
 	}
+
 
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		
 	}
 
-	public BigDecimal getSubtotal() {
-		return null;
+	public BigDecimal getSubtotal() {//liczy wartość faktury
+		if (products.isEmpty()){
+			return BigDecimal.ZERO;
+		}
+		else{
+			BigDecimal subTot = BigDecimal.ZERO;
+			for (Product product: products.keySet()){
+				
+				BigDecimal sum = new BigDecimal(products.get(product)).multiply(product.getPrice());
+				subTot=subTot.add(sum);
+			
+		 }
+			return subTot;
+		}
 	}
 
 	public BigDecimal getTax() {
+		if (products.isEmpty()){
+			return BigDecimal.ZERO;
+		}
+		else{
 		return null;
+		}
 	}
 
 	public BigDecimal getTotal() {
+		if (products.isEmpty()){
+			return BigDecimal.ZERO;
+		}
+		else{
 		return null;
+		}
 	}
 }
