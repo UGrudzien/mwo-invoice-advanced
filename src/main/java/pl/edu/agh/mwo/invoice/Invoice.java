73bf04c845ee4cf22f0fb6b5e68ddec9708ed3c7
Urszula Grudzień <Urszula Grudzie>
2017-02-22 15:39:16 +0100

    Invoice 4 test left

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -3,55 +3,91 @@ package pl.edu.agh.mwo.invoice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+
+import pl.edu.agh.mwo.invoice.product.OtherProduct;
 import pl.edu.agh.mwo.invoice.product.Product;
+import pl.edu.agh.mwo.invoice.product.TaxFreeProduct;
 
 public class Invoice {
+	
 	private HashMap<Product, Integer> products = new HashMap<Product, Integer>();
+
 	public void addProduct(Product product) {
-		if (products.containsKey(product)){
-			products.put(product, products.get(product)+1);
-		}
-		else{
+		// TODO: implement
+		if (products.containsKey(product)) {
+			products.replace(product, (products.get(product) + 1));
+		} else {
 			products.put(product, 1);
 		}
 	}
 
-
 	public void addProduct(Product product, Integer quantity) {
-		
+		if (products.containsKey(product)) {
+			products.replace(product, (products.get(product) + 1));
+		} else {
+			products.put(product, 1);
+		}
 	}
 
-	public BigDecimal getSubtotal() {//liczy wartość faktury
-		if (products.isEmpty()){
+	public BigDecimal getSubtotal() {
+		if (products.isEmpty()) {
 			return BigDecimal.ZERO;
+		} else {
+
+			BigDecimal subTotal = BigDecimal.ZERO;
+
+			for (Product product : products.keySet()) {
+
+				BigDecimal sum = product.getPrice().multiply(new BigDecimal(products.get(product)));
+
+				subTotal = subTotal.add(sum);
+			}
+			return subTotal;
 		}
-		else{
-			BigDecimal subTot = BigDecimal.ZERO;
-			for (Product product: products.keySet()){
-				
-				BigDecimal sum = new BigDecimal(products.get(product)).multiply(product.getPrice());
-				subTot=subTot.add(sum);
-			
-		 }
-			return subTot;
+
 		}
-	}
+
+	
 
 	public BigDecimal getTax() {
-		if (products.isEmpty()){
+		if (products.isEmpty()) {
 			return BigDecimal.ZERO;
-		}
-		else{
-		return null;
+		} else {
+			BigDecimal getTax = BigDecimal.ZERO;
+
+			for (Product product : products.keySet()) {
+				BigDecimal taxSum = product.getTaxPercent().multiply(new BigDecimal(products.get(product)))
+						.multiply(product.getPrice());
+				getTax = getTax.add(taxSum);
+
+			}
+			return getTax;
+
 		}
 	}
 
 	public BigDecimal getTotal() {
-		if (products.isEmpty()){
+		if (products.isEmpty()) {
 			return BigDecimal.ZERO;
 		}
-		else{
-		return null;
+		BigDecimal total = BigDecimal.ZERO;
+
+		for (Product product : products.keySet()) {
+			BigDecimal sum = product.getPriceWithTax().multiply(new BigDecimal(products.get(product)));
+			total = total.add(sum);
+
 		}
+		return total;
+	}
+	public static void main (String[]args){
+		
+		HashMap<Product, Integer> myProduct = new HashMap<Product, Integer>();
+		Product maslo = new OtherProduct("masloZ", new BigDecimal(10));
+		Product chleb = new TaxFreeProduct("chlebK", new BigDecimal(1));
+		myProduct.put( maslo, 2);
+		myProduct.put( chleb, 1);
+		Invoice invoice = new Invoice();
+		invoice.getSubtotal();
+		
 	}
 }
